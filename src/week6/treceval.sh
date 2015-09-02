13393210abdcbd293a46f62a14dc76c3ec9c8c56
Harry Scells <harryscells@gmail.com>
2015-09-02 18:32:25 +1000

    added bash scripts

---
+++ src/week6/treceval.sh
@@ -0,0 +1,14 @@
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_none_Tf.txt >> ~/Repositories/INB344/src/week6/ap8889_none_Tf_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_none_TF_IDF.txt >> ~/Repositories/INB344/src/week6/ap8889_none_TF_IDF_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_none_LemurTF_IDF.txt >> ~/Repositories/INB344/src/week6/ap8889_none_LemurTF_IDF_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_none_BB2.txt >> ~/Repositories/INB344/src/week6/ap8889_none_BB2_EVAL.txt
+
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_Tf.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_Tf_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_TF_IDF.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_TF_IDF_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_LemurTF_IDF.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_LemurTF_IDF_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_BB2.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_BB2_EVAL.txt
+
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_Tf.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_Tf_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_TF_IDF.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_TF_IDF_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_LemurTF_IDF.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_LemurTF_IDF_EVAL.txt
+share/trec_eval.9.0/trec_eval share/qrels.ap8889.all ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_BB2.txt >> ~/Repositories/INB344/src/week6/ap8889_stopwords_stemmer_BB2_EVAL.txt
